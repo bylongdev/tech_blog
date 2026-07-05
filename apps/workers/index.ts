698209dc1c2ede1376ec60032bca4bd7bbcff778
NightW0lfff <philong170101@gmail.com>
2026-07-05 22:50:10 +1000

    Add main loop

--- apps/workers/index.ts
+++ apps/workers/index.ts
@@ -2,21 +2,28 @@ import { registerSources } from "./jobs/register-sources.job.js";
 import { fetchSources } from "./jobs/fetch-sources.job.js";
 import { prisma } from "@techblog/database/src/client.js";
 
-import { metaDataExtractingJob } from "./jobs/meta-data-extracting.job.js";
-
 import { MetaDataExtractingAgent } from "./agents/extracting/meta-data-extracting.agent.js";
 const agent = new MetaDataExtractingAgent();
+
+const INTERVAL = 60 * 1000; // run every minute
 
 async function main() {
 	await registerSources();
 	await fetchSources();
 }
 
-main()
-	.catch((error) => {
+async function run() {
+	try {
+		await main();
+	} catch (error) {
 		console.error("Error in worker:", error);
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-		console.log("Worker finished.");
-	});
+	} finally {
+		console.log("-".repeat(30));
+		console.log("Worker cycle finished.\n");
+		console.log("-".repeat(30));
+	}
+
+	setTimeout(run, INTERVAL);
+}
+
+run();
