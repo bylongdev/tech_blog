695655651663dcf25cf31f6d72113bb81e0c3a68
bylongdev <philong170101@gmail.com>
2026-07-17 10:59:24 +1000

    Restructure the route

--- apps/frontend/app/dashboard/articles/page.tsx
+++ apps/frontend/app/dashboard/articles/page.tsx
@@ -1,14 +1,13 @@
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
-import ArticlesTable from "../overview/articles/article-table";
+import ArticlesTable from "./article-table/index";
 
-export default function DashboardPage() {
+export default function ArticlesPage() {
 	return (
 		<div className="flex h-full w-full items-center justify-center">
 			<Card className="dark:bg-zinc-600/20 flex flex-1 h-full">
 				<CardContent className="flex flex-col items-center flex-1 justify-center gap-4">
-					{/* <ArticlesTable /> */}
-					test
+					<ArticlesTable />
 				</CardContent>
 			</Card>
 		</div>
