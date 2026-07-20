a7335e1e644017202a7e4ebd8f5921d412d9bd0a
bylongdev <philong170101@gmail.com>
2026-07-21 09:22:49 +1000

    Add debug code.

--- apps/frontend/app/api/auth/register/route.ts
+++ apps/frontend/app/api/auth/register/route.ts
@@ -15,6 +15,8 @@ export async function POST(request: Request) {
 
 	const data = await response.json();
 
+	console.log("Response from server:", data);
+
 	const nextResponse = NextResponse.json(data, {
 		status: response.status,
 	});
